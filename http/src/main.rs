0540fba1302b1324a9993f9fcd21c37f487c064f
Naoki Ikeguchi <me@s6n.jp>
2023-02-11 01:41:18 +0900

    feat(http): Initial HTTP threaded server

--- http/src/main.rs
+++ http/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+mod pool;
+
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+use anyhow::Result;
+use hyper::server::conn::Http;
+use hyper::service::service_fn;
+use hyper::{Body, Request, Response};
+use tokio::net::TcpListener;
+
+use crate::pool::Pool;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<()> {
+    let mut pool = Pool::new(4);
+
+    pool.spawn_many(3, |stream| async move {
+        if let Err(http_err) = Http::new()
+            .serve_connection(stream, service_fn(hello))
+            .await
+        {
+            eprintln!("Error while serving HTTP connection: {http_err}");
+        }
+    });
+
+    let addr: SocketAddr = ([127, 0, 0, 1], 8080).into();
+    let listener = TcpListener::bind(addr).await?;
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+        pool.send(stream)?;
+    }
+}
+
+async fn hello(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new(Body::from("Hello World!")))
 }
---
+++ http/src/pool.rs
@@ -0,0 +1,85 @@
+use std::future::Future;
+use std::marker::PhantomData;
+use std::thread::{spawn, JoinHandle};
+
+use anyhow::Result;
+use crossbeam_channel::{bounded, Receiver, Sender};
+use tokio::runtime::Builder;
+
+pub struct Thread<C> {
+    handle: JoinHandle<()>,
+    _phantom: PhantomData<fn() -> C>,
+}
+
+impl<C> Thread<C>
+where
+    C: Send + 'static,
+{
+    pub fn spawn<F, Fut>(rx: Receiver<C>, f: F) -> Self
+    where
+        F: Fn(C) -> Fut + Send + 'static,
+        Fut: Future<Output = ()>,
+    {
+        Self {
+            handle: spawn(move || {
+                Builder::new_current_thread()
+                    .enable_all()
+                    .build()
+                    .unwrap()
+                    .block_on(async move {
+                        loop {
+                            f(rx.recv().unwrap()).await
+                        }
+                    })
+            }),
+            _phantom: PhantomData,
+        }
+    }
+
+    #[allow(unused)]
+    pub fn join(self) -> std::thread::Result<()> {
+        self.handle.join()
+    }
+}
+
+pub struct Pool<C> {
+    threads: Vec<Thread<C>>,
+    channel: (Sender<C>, Receiver<C>),
+}
+
+impl<C> Pool<C>
+where
+    C: Send + Sync + 'static,
+{
+    pub fn new(capacity: usize) -> Self {
+        Self {
+            threads: Vec::new(),
+            channel: bounded(capacity),
+        }
+    }
+
+    pub fn spawn<F, Fut>(&mut self, f: F)
+    where
+        F: Fn(C) -> Fut + Send + 'static,
+        Fut: Future<Output = ()>,
+    {
+        let (_, rx) = &self.channel;
+        self.threads.push(Thread::spawn(rx.clone(), f));
+    }
+
+    pub fn spawn_many<F, Fut>(&mut self, count: usize, f: F)
+    where
+        F: Fn(C) -> Fut + Clone + Send + 'static,
+        Fut: Future<Output = ()>,
+    {
+        [0..count].into_iter().for_each(|_| {
+            self.spawn(f.clone());
+        })
+    }
+
+    pub fn send(&self, connection: C) -> Result<()> {
+        let (tx, _) = &self.channel;
+        tx.try_send(connection)?;
+        Ok(())
+    }
+}
