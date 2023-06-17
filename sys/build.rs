34b996c46af58172c9da50f3b6d258dab1c9da6d
Natsuki Ikeguchi <me@s6n.jp>
2023-06-17 21:05:46 +0900

    feat: Link to libphp.so in rusty-php-sys

---
+++ sys/build.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("cargo:rustc-link-search=native=/usr/local/lib");
+    println!("cargo:rustc-link-lib=dylib=php");
+    // println!("cargo:rustc-link-lib=dylib=ssl");
+    // println!("cargo:rustc-link-lib=dylib=crypto");
+    // println!("cargo:rustc-link-lib=dylib=readline");
+    // println!("cargo:rustc-link-lib=dylib=argon2");
+    // println!("cargo:rustc-link-lib=dylib=curl");
+    // println!("cargo:rustc-link-lib=dylib=onig");
+    // println!("cargo:rustc-link-lib=dylib=z");
+}
